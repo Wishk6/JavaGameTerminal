67bd1fb0e2cf0d4300303425288d2799ec5c8a2d
GSALVI <gaetan.salvi@ynov.com>
2021-10-19 15:26:11 +0200

    add Asker static class

--- src/Game/Misc/Asker.java
+++ src/Game/Misc/Asker.java
@@ -4,19 +4,103 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Asker {
-    public static int AskChoice(List<String> Choices, String Question) {
+    public static int askChoice(List<String> choices, String question) {
         int result = 0;
+
+        println(question);
+
+        for(int i = 0; i < choices.size(); i++){
+            println(i+1 + ". " + choices.get(i));
+        }
+
+        while (result <= 0 || result > choices.size()){
+            try {
+                result = Integer.parseInt(scanner());
+            }catch (Exception e){
+                result = 0;
+            }
+        }
+
+        clear();
         return result;
     }
 
-    public static String AskEntry(String Question)
-    {
-        Scanner scanner = new Scanner(System.in);
-        return "";
+    public static String askEntry(String question){
+        String result = "";
+        boolean confirmed = false;
+
+
+
+        while (!confirmed){
+            println(question);
+            result = scanner();
+            confirmed = confirmChoice();
+        }
+
+        clear();
+        return result;
     }
 
-    public static boolean ConfirmChoice()
-    {
-        return true;
+    private static boolean confirmChoice(){
+        boolean result = false;
+        String val = "";
+
+        while (!val.equals("o") && !val.equals("O") && !val.equals("n") && !val.equals("N")){
+            println("Are you sure ? (O/N)");
+            val = scanner();
+            if (val.equals("o") || val.equals("O")) result = true;
+        }
+        return result;
+    }
+
+    private static String scanner(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Your choice: ");
+        return scan.nextLine();
+    }
+
+    public static void println(String val){
+        System.out.println(val);
+    }
+
+    public static void clear(){
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
+        println("");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
-import Game.Units.Character;
-import Game.Enums.WeaponClass;
+import Game.Misc.Asker;
 
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args)
     {
-        Character Nanaka = new Character("Nanaka", WeaponClass.ShieldSword);
-        Character Jaetan = new Character("Jaetan", WeaponClass.Bow);
-        Character XFactors = new Character("XFactors", WeaponClass.Staff);
-        Character Wishk = new Character("Wishk", WeaponClass.LongSword);
-
-
-        //Combat Jaetan Nanaka Dart vs LUA\\
-
-        System.out.println("Bienvenue ce soir dans le combat qui opposera C# et Dart.\nC# sera représenté par Nanaka.\nDart sera représenté par Jaetan.");
-        while(Jaetan.isDead() || Nanaka.isDead())
-        {
-
-
-        }
     }
 }
 
