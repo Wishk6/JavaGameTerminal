6508f6f2a817082c72fdc45b30ac84da9e04914d
GSALVI <gaetan.salvi@ynov.com>
2021-11-09 11:29:39 +0100

    create IEntity

---
+++ src/Game/Units/IEntity.java
@@ -0,0 +1,14 @@
+package Game.Units;
+
+import Game.Items.IWeapon;
+
+public interface IEntity {
+    String getName();
+    String getType();
+    float getHp();
+    float getAttack();
+    void attack(IEntity target);
+    void defense(IEntity attacker ,IWeapon weapon ,float damage);
+    float getDefense();
+    IWeapon getMainWeapon();
+}
