49ce8a6641ddfa21d75dbd59afb2822ab56dda3f
GSALVI <gaetan.salvi@ynov.com>
2021-11-16 08:35:28 +0100

    remove bad comm

--- src/Game/Units/Entity.java
+++ src/Game/Units/Entity.java
@@ -6,57 +6,6 @@ import Game.Items.WeaponBag;
 import java.util.ArrayList;
 import java.util.List;
 
-/*public abstract class Entity implements IEntity {
-    protected String name;
-    protected int level;
-    protected int hp;
-    protected int attackDamage;
-    protected WeaponBag weapons = new WeaponBag();
-
-
-    protected Entity(String _name, int _level, int _hp, int _attackDamage){
-        name = _name;
-        level = _level;
-        hp = _hp;
-        attackDamage = _attackDamage;
-    }
-
-    public void attack(Entity entity){
-        entity.defend(attackDamage);
-    }
-    public void attack(Entity entity, int weaponId){
-        IWeapon weapon = getWeaponById(weaponId);
-
-        entity.defend(attackDamage + weapon.getDamage());
-        if(weapon.getUsure() >= 100){
-            weapons.remove(weaponId);
-            Asker.println("Weapon " + weapon.getName() + " will be removed because it is too ruined.");
-        }
-    }
-    public void defend(int damage){
-        hp -= damage;
-    }
-
-    //Getters
-    public List<String> getWeaponNames(){
-        List<String> names = new ArrayList<>();
-
-        for(IWeapon weapon : weapons){
-            names.add(weapon.getName());
-        }
-        return names;
-    }
-    private IWeapon getWeaponById(int id){
-        return weapons.get(id);
-    }
-    public String getName(){ return name; }
-    public float getHp(){ return hp; }
-    public int getAttackDamage(){ return attackDamage; }
-    public boolean isAlive(){
-        return hp > 0;
-    }
-}*/
-
 public abstract class Entity implements IEntity {
     protected String name;
     protected float hp;
